7ad3a89f6b1daa93e56a8587b66c5a3d9c09dbf2
Mitchell Walker <eagleye1144@gmail.com>
2021-01-22 11:08:00 -0700

    Task 5 complete

--- index.js
+++ index.js
@@ -266,8 +266,10 @@ function get20s(artists){
  
  For example, if removeArtist is invoked with the artists array and the number 0, it will remove Amedeo Modigliani from our dataset and return the number 19. */
 
-function removeArtist(/*Your Code Here*/){
+function removeArtist(artist, int){
    /*Your Code Here*/
+   artist.splice(int, 1)
+   return artist.length;
 }
    
 
